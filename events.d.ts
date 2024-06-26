78f0946d038aaf787e9c4c982b7feac508b4d501
blackshibe <kacperszczepan@tutanota.com>
2024-06-27 00:12:47 +0200

    events definitions aren't needed. remove them

--- events.d.ts
+++
@@ -1,9 +0,0 @@
-/*
-    Typings for Teardown Lua API 1.5
-    by blackshibe, written 26/06/2024
-*/
-
-declare function init(): void;
-declare function tick(dt: number): void;
-declare function update(dt: number): void;
-declare function draw(): void;
